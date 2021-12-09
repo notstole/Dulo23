543afd6a4dd295e9421a1bd0ad360d86930156d8
notstole <93998864+notstole@users.noreply.github.com>
2021-12-09 13:23:13 +0100

    Delete commands directory

--- commands/daily.js
+++
@@ -1,7 +0,0 @@
-module.exports.execute = async (client, message, args) => {
-    let amount = Math.floor(Math.random() * 500) + 100;
-    let addMoney = client.eco.daily(client.ecoAddUser, amount);
-    if (addMoney.onCooldown) return message.reply(`Vec Uzeo dnevnu nagradu ${addMoney.time.hours} hours, ${addMoney.time.minutes} minutes & ${addMoney.time.seconds} seconds to claim it again.`);
-    else return message.reply(`Ti si uzeo dnevnu nagradu**${addMoney.amount}** 💸  **${addMoney.after}** 💸.`);
-};
-
