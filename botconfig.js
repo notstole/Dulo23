120c34d6e15f41a01068e8644c8f7acaf4d1efe5
notstole <93998864+notstole@users.noreply.github.com>
2021-12-09 13:22:07 +0100

    Delete botconfig.js

--- botconfig.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    token: "OTE4NDYzNDE4NDE0MDE0NTM1.YbHntw.mDh0NFjTAGkXsPsEhXQWRlS1QMY",
-    prefix: "!",
-    admins: [
-        "682206545907548169"
-],
-    debug: true,
-    countChannel: "countChannelID"
-};
